fe5099180f2b2c17394c8e11dacade8ab14b8b4b
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2025-01-07 00:10:31 -0800

    Testing photon visiom simulated

--- src/main/java/overture/sim/SimMain.java
+++ src/main/java/overture/sim/SimMain.java
@@ -1,32 +1,20 @@
 package overture.sim;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.units.DistanceUnit;
+
 
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.seasonspecific.crescendo2024.Arena2024Crescendo;
 import org.ironmaple.simulation.seasonspecific.crescendo2024.CrescendoNoteOnField;
 import org.littletonrobotics.junction.Logger;
-import org.photonvision.PhotonCamera;
-import org.photonvision.simulation.PhotonCameraSim;
-import org.photonvision.simulation.SimCameraProperties;
-import org.photonvision.simulation.VisionSystemSim;
 import overture.sim.swerve.SwerveChassis;
-import static edu.wpi.first.units.Units.Inches;
 
 public class SimMain {
     SimulatedArena arena = null;
     SwerveChassis chassis = null;
-    VisionSystemSim visionSim = new VisionSystemSim("main");
-
-    PhotonCamera camera = new PhotonCamera("Global_Shutter_Camera");
 
     public void Initialize() {
         // Mechanisms
@@ -34,9 +22,6 @@ public class SimMain {
 
         // Arena
         SimulatedArena.overrideInstance(new Arena2024Crescendo());
-        AprilTagFieldLayout tagLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
-        visionSim.addAprilTags(tagLayout);
-        AddSimCameras();
         arena = SimulatedArena.getInstance();
 
         // Add game pieces to Arena
@@ -46,27 +31,10 @@ public class SimMain {
         arena.addDriveTrainSimulation(chassis);
     }
 
-    public void AddSimCameras() {
-        SimCameraProperties cameraProp = new SimCameraProperties();
-        // The PhotonCamera used in the real robot code.
-        // The simulation of this camera. Its values used in real robot code will be updated.
-        PhotonCameraSim cameraSim = new PhotonCameraSim(camera, cameraProp);
-
-        // Our camera is mounted 0.1 meters forward and 0.5 meters up from the robot pose,
-        // (Robot pose is considered the center of rotation at the floor level, or Z = 0)
-        Translation3d robotToCameraTrl = new Translation3d(Inches.of(-14.950771), Inches.of(0), Inches.of(14.034697));
-        // and pitched 15 degrees up.
-        Rotation3d robotToCameraRot = new Rotation3d(0, Math.toRadians(-30), Math.toRadians(180.0));
-        Transform3d robotToCamera = new Transform3d(robotToCameraTrl, robotToCameraRot);
-
-        // Add this camera to the vision system simulation with the given robot-to-camera transform.
-        visionSim.addCamera(cameraSim, robotToCamera);
-    }
 
     public void Periodic() {
         arena.simulationPeriodic();
         chassis.Update();
-        visionSim.update(chassis.getSimulatedDriveTrainPose());
 
         Logger.recordOutput("FieldSimulation/RobotPosition", chassis.getSimulatedDriveTrainPose());
         Logger.recordOutput("FieldSimulation/Notes", arena.getGamePiecesArrayByType("Note"));
