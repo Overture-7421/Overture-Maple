f1985581bf955b7d4c243f88ce29f5d94e56215b
Luis Leyva <luisoleyvan@gmail.com>
2025-01-07 18:37:33 -0600

    Allow for pathplanner pose override

--- src/main/java/overture/sim/SimMain.java
+++ src/main/java/overture/sim/SimMain.java
@@ -1,18 +1,26 @@
 package overture.sim;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
+import java.util.EnumSet;
 
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.seasonspecific.reefscape2025.Arena2025Reefscape;
 import org.ironmaple.simulation.seasonspecific.reefscape2025.ReefscapeCoralAlgaeStack;
 import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.NetworkTableEvent;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTableListener;
+import edu.wpi.first.networktables.StructArraySubscriber;
+import edu.wpi.first.wpilibj.DriverStation;
 import overture.sim.swerve.SwerveChassis;
 
 public class SimMain {
-	SimulatedArena arena = null;
-	SwerveChassis chassis = null;
+	static SimulatedArena arena = null;
+	static SwerveChassis chassis = null;
+
+	NetworkTableListener autoLoad = null;
 
 	public void Initialize() {
 		// Mechanisms
@@ -29,6 +37,25 @@ public class SimMain {
 
 		// Add mechanisms to Arena
 		arena.addDriveTrainSimulation(chassis);
+
+		autoLoad = NetworkTableListener.createListener(
+				NetworkTableInstance.getDefault().getStringTopic("/PathPlanner/activePath"),
+				EnumSet.of(NetworkTableEvent.Kind.kValueRemote), SimMain::handleAutoChange);
+	}
+
+	static public void handleAutoChange(NetworkTableEvent event) {
+		try {
+			StructArraySubscriber<Pose2d> initPoseArray = event.getInstance().getTable("PathPlanner")
+					.getStructArrayTopic("activePath", Pose2d.struct).subscribe(new Pose2d[1]);
+			Pose2d initPose = initPoseArray.get()[0];
+
+			chassis.setSimulationWorldPose(initPose);
+			arena.resetFieldForAuto();
+			DriverStation.reportWarning("Auto Loaded!", false);
+
+		} catch (Exception e) {
+			DriverStation.reportError("Can't update robot Pose2d", false);
+		}
 	}
 
 	public void Periodic() {
